70c104964d2e870dc81f6f32e9f7f39d21cf00ef
dhrrruvin <dhruvinishere@gmail.com>
2023-07-10 18:56:24 +0530

    added route controllers

--- Server/controller/find_trains.js
+++ Server/controller/find_trains.js
@@ -1,15 +1,120 @@
-const path = require("path");
+const Station = require("../models/station");
+const TrainRoute = require("../models/trainRoute");
+const Train1 = require("../models/train");
 
-const find_trains = (req, res) => {
-  // const source = req.body.source;
-  // const destination = req.body.destination;
+const handleResult = (result) => {
+  let trainsBetweenStation = [];
+  for (i = 0; i < result.length - 1; i += 2) {
+    trainsBetweenStation.push([result[i], result[i + 1]]);
+  }
 
+  console.log("final result: ", trainsBetweenStation.length);
+  // res.status(200).send(trainsBetweenStation.length.toString());
+  return trainsBetweenStation;
+};
+
+// const sendData = (result, res) => {
+//   const batchSize = 5;
+
+//   const readble = new require("stream").Readable({ objectMode: true });
+
+//   const batches = [];
+//   for (let i = 0; i < result.length; i += batchSize) {
+//     const batch = result.slice(i, i + batchSize);
+//     batches.push(batch);
+//   }
+
+//   readble.push(null);
+
+//   res.setHeader("Content-Type", "application/json");
+//   res.setHeader("Transfer-Encoding", "chunked");
+
+//   readble.pipe(res);
+// };
+
+const find_trains = async (req, res) => {
   const source = req.query.src;
   const destination = req.query.dest;
 
-  // console.log(source);
+  console.log(source);
+  console.log(destination);
 
-  res.sendFile(path.join(__dirname, "../public", "train_results.html"));
+  q = {
+    $and: [
+      { stations: source },
+      { stations: destination },
+      {
+        $expr: {
+          $lt: [
+            { $indexOfArray: ["$stations", source] },
+            { $indexOfArray: ["$stations", destination] },
+          ],
+        },
+      },
+    ],
+  };
+
+  projection = { _id: 0, train_number: 1 };
+
+  // returns the list of train numbers from A->B
+  TrainRoute.find(q, projection)
+    .exec()
+    .then((resp) => extractAndSearch(resp));
+
+  const extractAndSearch = (object) => {
+    let available_trains = [];
+    let result = [];
+    // console.log(object);
+    // extract the trains from the json object
+    object.filter((obj) => available_trains.push(obj.train_number));
+
+    // console.log(available_trains);
+
+    // get the FROM station id
+    let from_id;
+    Station.findOne({ station_code: source })
+      .then((station) => {
+        from_id = station.id;
+      })
+      .catch((e) => console.log(e));
+
+    // get the TO station id
+    Station.findOne({ station_code: destination })
+      .then((station) => {
+        // when you have both {from} and {to} station id
+        // find all the trains with {available_trains} which also have station field as {from} or {to}
+        // this will give all the details of the trains on searched stations
+
+        to_id = station.id;
+        q = {
+          $and: [
+            { train_number: { $in: available_trains } },
+            { $or: [{ "route.station": from_id }, { "route.station": to_id }] },
+          ],
+        };
+        p = {
+          train_number: 1,
+          train_name: 1,
+          "route.station": 1,
+          "route.arrival_time": 1,
+          "route.departure_time": 1,
+        };
+        Train1.find(q, p)
+          .exec()
+          .then((route) => {
+            result.push(route);
+            console.log("total results: ", result[0].length);
+
+            // this method will make pair of {source} and {destination} train details to send this results to front-end
+            let trainsBetweenStations = handleResult(result[0]);
+
+            res.status(200).send(trainsBetweenStations);
+
+            // sendData(trainsBetweenStations, res);
+          });
+      })
+      .catch((e) => console.log(e));
+  };
 };
 
 module.exports = find_trains;
---
+++ Server/controller/seat_count.js
@@ -0,0 +1,84 @@
+const { getConnection, releaseConnection } = require("../config/mysqlDB");
+
+const get_seat_count = async (req, res) => {
+  const source = req.query.src;
+  const destination = req.query.dest;
+  const train_number = req.query.train_number;
+
+  if (!source || !destination || !train_number) {
+    console.log("error getting the query params");
+    console.log("train_number: " + train_number);
+    console.log("source: " + source);
+    console.log("destination: " + destination);
+  }
+
+  let connection;
+
+  try {
+    connection = await getConnection();
+    connection.beginTransaction();
+  } catch (err) {
+    console.log("error getting connection");
+    console.error(err);
+  }
+
+  let response, start_id, end_id;
+
+  const get_rows = new Promise((resolve, reject) => {
+    connection.query(
+      `select id from book_seats where train_number="${train_number}" and (source_station_code="${source}" or destination_station_code="${destination}")`,
+      (err, res) => {
+        if (err) reject(err);
+        else resolve(res);
+      }
+    );
+  });
+
+  try {
+    response = await get_rows;
+    if (response.length == 1) {
+      start_id = end_id = response[0].id;
+    } else {
+      start_id = response[0].id;
+      end_id = response[1].id;
+    }
+  } catch (error) {
+    console.log("error in get_rows in seats_count.js");
+    console.log(error);
+  }
+
+  const get_count = new Promise((resolve, reject) => {
+    connection.query(
+      `select seats from book_seats where id between ${start_id} and ${end_id}`,
+      (err, res) => {
+        if (err) reject(err);
+        else resolve(res);
+      }
+    );
+  });
+
+  try {
+    response = await get_count;
+    let min_seat_count = Number.MAX_VALUE;
+    for (i = 0; i < response.length; i++) {
+      min_seat_count = Math.min(min_seat_count, response[i].seats);
+    }
+
+    try {
+      connection.commit();
+      releaseConnection(connection);
+      // console.log("connecxn released!");
+    } catch (error) {
+      console.log("error commiting data");
+      console.error(error);
+      releaseConnection(connection);
+    }
+
+    res.status(200).send(min_seat_count.toString());
+  } catch (err) {
+    console.log("error fetching the seats count!");
+    console.error(err);
+  }
+};
+
+module.exports = get_seat_count;
